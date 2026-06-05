fba50bba58c52b613810ef05a3c0a0a7547b798f
JDNQ <108049503+JDNQ@users.noreply.github.com>
2026-06-05 20:46:10 +0700

    1

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
+import { PrismaModule } from "../prisma/prisma.module";
 import { OrdersController } from "./orders.controller";
 import { OrdersService } from "./orders.service";
 import { VouchersModule } from "../vouchers/vouchers.module";
 
 @Module({
-  imports: [VouchersModule],
+  imports: [VouchersModule, PrismaModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
--- src/vouchers/vouchers.module.ts
+++ src/vouchers/vouchers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { PrismaModule } from "../prisma/prisma.module";
 import { VouchersController } from "./vouchers.controller";
 import { VouchersService } from "./vouchers.service";
 
 @Module({
+  imports: [PrismaModule],
   controllers: [VouchersController],
   providers: [VouchersService],
   exports: [VouchersService],
